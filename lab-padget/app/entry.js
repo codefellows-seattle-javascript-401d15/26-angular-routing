bf7a81e9b0700c004e7cf49a0b26d300d3cb42fb
Michael Padget <mpadget@comcast.net>
2017-05-30 21:12:13 -0700

    Project scaffolding

---
+++ lab-padget/app/entry.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// TODO: use require.context to add all of your angular construct definitions.
+
+// require('./scss/main.scss')
+
+const path = require('path');
+//const camelcase = require('camelcase');
+const pascalcase = require('pascalcase');
+const angular = require('angular');
+require('@uirouter/angularjs');
+
+const routesApp = angular.module('routesApp', ['ui.router']);
+
+let context = require.context('./config/', true, /\.js$/);
+context.keys().forEach(key => routesApp.config(context(key)));
+
+context = require.context('./view/', true, /\.js$/);
+context.keys().forEach(key => {
+  let name = pascalcase(path.basename(key, '.js'));
+  routesApp.controller(name, context(key));
+});
---
+++ lab-padget/test/gallery-controller-test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// TODO: Write a test to ensure that the title property is set for each controller
+/*
+/test/home-controller-test.js
+/test/gallery-controller-test.js
+/test/signup-controller-test.js
+*/
