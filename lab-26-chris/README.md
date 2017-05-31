93b71d2fc7109174e43840af0fd2a56269b4a2df
Chris Closser <chrisclosser1@gmail.com>
2017-05-31 12:24:51 -0700

    cant get karma to work

---
+++ lab-26-chris/README.md
@@ -0,0 +1,11 @@
+Lab 26 - Angular-Routing
+======
+#### My git hub repo:
+   - you can clone or fork from here
+   - https://github.com/ChristopherSClosser/26-angular-routing
+
+#### Get started
+  - Clone or download
+  - npm i
+  - npm run watch
+  - click on the link in your terminal or open your browser and go to the local port specified
---
+++ lab-26-chris/app/entry.js
@@ -0,0 +1,19 @@
+'use strict';
+
+require('@uirouter/angularjs');
+// require('./scss/main.scss');
+const path = require('path');
+// const camelcase = require('camelcase');
+const pascalcase = require('pascalcase');
+const angular = require('angular');
+
+const routesApp = angular.module('routesApp', ['ui.router']);
+
+let context = require.context('./config/', true, /\.js$/);
+context.keys().forEach(key => routesApp.config(context(key)));
+
+context = require.context('./view/', true, /\.js$/);
+context.keys().forEach(key => {
+  let name = pascalcase(path.basename(key, '.js'));
+  routesApp.controller(name, context(key));
+});
